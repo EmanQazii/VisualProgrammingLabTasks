02dabaaed8d1d94e1769d3a636565d2110af9ee3
EmanQazii <168670629+EmanQazii@users.noreply.github.com>
2024-10-16 10:45:36 -0700

    Add files via upload

---
+++ lab4/Exercise1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person()
+    {
+        Name = null;
+    }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Student : Person
+{
+    public string RegNo { get; set; }
+    public int Age { get; set; }
+    public string Program { get; set; }
+
+    public Student() : base()
+    {
+        RegNo = null;
+        Age = 0;
+        Program = null;
+    }
+
+    public Student(string name, string regNo, int age, string program) : base(name)
+    {
+        RegNo = regNo;
+        Age = age;
+        Program = program;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student();
+        Student student2 = new Student("John", "BSCS123", 20, "Computer Science");
+
+        student1.Name = "Alice";
+        student1.RegNo = "BSIT456";
+        student1.Age = 21;
+        student1.Program = "Information Technology";
+
+        Console.WriteLine($"Student 1: Name = {student1.Name}, RegNo = {student1.RegNo}, Age = {student1.Age}, Program = {student1.Program}");
+        Console.WriteLine($"Student 2: Name = {student2.Name}, RegNo = {student2.RegNo}, Age = {student2.Age}, Program = {student2.Program}");
+    }
+}
+
